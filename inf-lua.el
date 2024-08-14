75c81651638230242a16936c9af5f040e6fcad47
Noah Peart <noah.v.peart@gmail.com>
2024-08-13 18:01:18 -0700

    keywords

--- inf-lua.el
+++ inf-lua.el
@@ -5,7 +5,7 @@
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "29.1"))
 ;; Created:  9 December 2023
-;; Keywords: lua, languages
+;; Keywords: lua repl languages
 
 ;; This file is not part of GNU Emacs.
 ;;
